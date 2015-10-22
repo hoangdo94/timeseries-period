1fe69d5cb37a41630e4f3a0a3e0813a34a93b78e
Hoang Do <hoangdo.dh94@gmail.com>
2015-10-22 10:38:12 +0700

    add heuristic algorithm

--- js/main.js
+++ js/main.js
@@ -13,7 +13,7 @@ function support(timeseries, p, s) {
 function findPeriod(timeseries, threshold) {
     var n = timeseries.length;
     for (var p = 1; p <= n / 2; p++) {
-        for (var s = 0; s < n - p; s++) {
+        for (var s = 0; s < p; s++) {
             var c = support(timeseries, p, s);
             if (100 * c > threshold) {
                 return {
@@ -26,11 +26,53 @@ function findPeriod(timeseries, threshold) {
     }
 }
 
+function findPeriodHeur(timeseries, threshold) {
+    var n = timeseries.length;
+    var currBestP = {
+        period: 0,
+        score: 0,
+    };
+    //loop p
+    for (var p = 1; p <= n / 2; p++) {
+        //heuristic score
+        var heurScore = 0;
+        for (var i = 0; i < n - p; i++) {
+            if (timeseries[i] === '1' && timeseries[p + i] === '1') heurScore++;
+        }
+        heurScore = heurScore/(n-p);
+        if (heurScore > currBestP.score) {
+            currBestP = {
+                period: p,
+                score: heurScore
+            }
+        }
+    }
+    //loop s
+    var p = currBestP.period;
+    for (var s = 0; s < p; s++) {
+        var c = support(timeseries, p, s);
+        if (100 * c > threshold) {
+            return {
+                p: p,
+                s: s,
+                c: c,
+            }
+        }
+    }
+}
+
+
 $('#input-form').submit(function(e) {
     e.preventDefault();
     var timeseries = $('#input-string').val();
     var threshold = parseInt($('#input-threshold').val());
-    var result = findPeriod(timeseries, threshold);
+    var algorithm = $('#input-algorithm').val();
+    if (algorithm == 'bf') {
+       var result = findPeriod(timeseries, threshold);
+    } else {
+        var result = findPeriodHeur(timeseries, threshold);
+    }
+    
     if (result) {
         $('#result').empty()
             .append('<p>Period: ' + result.p + '</p>')
@@ -38,6 +80,6 @@ $('#input-form').submit(function(e) {
             .append('<p>Support: ' + Math.round(result.c * 10000) / 100 + '%</p>');
     } else {
         $('#result').empty()
-            .append('<p>Cannot find Period with ' + $('#input-threshold') + '% threshold</p>');
+            .append('<p>Cannot find Period with ' + threshold + '% threshold</p>');
     }
 });
